dde2a00fbe2621486a32bafa1e838aea30332958
Piyush7408407994 <piyushmishra7408407994@gmail.com>
2025-10-31 10:46:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function minDate(dates) {
-  //write you code here
+      if (dates.length === 0) return "";
+
+      const copy = [...dates];
+
+      copy.sort();
+
+      return copy[0];
 }
 
 // Do not change the code
@@ -37,4 +43,4 @@ var dates = [
   "2023/03/30",
 ];
 
-alert(minDate(dates));
+//alert(minDate(dates));
